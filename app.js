b5dff8e79e816f99faa30e85d3674d69fbec74b5
ManuRomanelli <g4m3r4k1ds@gmail.com>
2021-09-22 15:37:19 -0300

    archivos html 1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const publicPath = path.join(__dirname, './public');
 app.use(express.static('publicPath'));
 
-app.listen(4000,  () => {
+app.listen(4050,  () => {
     console.log("Levantando un servidor con Express")
 });
 
