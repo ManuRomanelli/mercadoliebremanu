b07b6080330df7980714705a423f738eaff42bf3
ManuRomanelli <manuelromanelli@hotmail.com>
2021-10-11 21:14:57 -0300

    actualizacion

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require ('express');
-const app = express();
 const path = require('path');
+const app = express();
 
-const publicPath = path.join(__dirname, './public');
-app.use(express.static('publicPath'));
 
-app.listen(4050,  () => {
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(3030,  () => {
     console.log("Levantando un servidor con Express")
 });
 
 app.get('/',(req,res) => {
-    res.sendFile(__dirname, './views/home.html')
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
 });
